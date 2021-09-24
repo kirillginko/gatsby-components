3944a972ccde7425bfb5e7d86e06bfba44a7c468
Kirill Ginko <kirillginko@gmail.com>
2021-09-23 23:57:58 -0400

    added animated background

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,23 @@ import "../styles/Global.css"
 export default function Home() {
   return (
     <>
-      <section className="nav">
-        <div>
-          <h1>Modern World</h1>
-        </div>
-        <div>
-          <h1>Future World</h1>
-        </div>
-      </section>
-      <section className="hero">
-        <div className="hero__title">
-          <h1>
-            Welcome to our <span>Neo/Retro</span> Future
-          </h1>
-        </div>
-      </section>
+      <div className="container">
+        <section className="nav">
+          <div>
+            <h1>Modern World</h1>
+          </div>
+          <div>
+            <h1>Future World</h1>
+          </div>
+        </section>
+        <section className="hero">
+          <div className="hero__title">
+            <h1>
+              Welcome to our <span>Neo/Retro</span> Future
+            </h1>
+          </div>
+        </section>
+      </div>
     </>
   )
 }
