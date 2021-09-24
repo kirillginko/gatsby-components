051b4f24f0a668af992e895b43935d8826ed3db2
Kirill Ginko <kirillginko@gmail.com>
2021-09-23 23:18:27 -0400

    started components project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
+import "../styles/Global.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <section className="nav">
+        <div>
+          <h1>Modern World</h1>
+        </div>
+        <div>
+          <h1>Future World</h1>
+        </div>
+      </section>
+      <section className="hero">
+        <div className="hero__title">
+          <h1>
+            Welcome to our <span>Neo/Retro</span> Future
+          </h1>
+        </div>
+      </section>
+    </>
+  )
 }
